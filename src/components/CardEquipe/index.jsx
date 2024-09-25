15f9679c0a6f45cff1d566da19d0bdfaf58d02bf
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-09-25 14:59:43 -0300

    cards equipes dinamico

--- src/components/CardEquipe/index.jsx
+++ src/components/CardEquipe/index.jsx
@@ -1,33 +1,34 @@
 import Trofeu from '../../assets/cards_equipes/trofeu.svg'
 import Podio from '../../assets/cards_equipes/podio.svg'
 import Bandeirinha from '../../assets/cards_equipes/bandeirinha.svg'
-import ABT from '../../assets/Times FE/ABT/ABT.SVG'
-import ABTCarro from '../../assets/Times FE/ABT/ABT Carro.png'
-import Fundo from '../../assets/fundo.svg'
+import equipes from '../../mocks/equipes'
 
 
 const CardEquipe = () => {
+
     return (  
         <>
+        {equipes.map((equipe) => (
              <div className="flex flex-col items-center px-6 py-6 rounded-xl w-full gap-4 relative z-10 overflow-hidden card-equipe">
                 <div className='flex justify-around w-full'>
                     <div>
                         <ul className='flex flex-col gap-3'>
-                            <li className='lista-card-piloto'><img src={Trofeu} /> 14</li>
-                            <li className='lista-card-piloto'><img src={Podio} /> 47</li>
-                            <li className='lista-card-piloto'><img src={Bandeirinha} /> 115</li>
+                            <li className='lista-card-piloto' style={{ color: equipe.cor }}><img src={Trofeu} /> {equipe.vitorias}</li>
+                            <li className='lista-card-piloto' style={{ color: equipe.cor }}><img src={Podio} /> {equipe.podios}</li>
+                            <li className='lista-card-piloto' style={{ color: equipe.cor }}><img src={Bandeirinha} /> {equipe.corridas}</li>
                         </ul>
                     </div>
                     <div className='flex items-end'>
-                        <img src={ABT} alt="" />
+                        <img src={equipe.time} alt="" />
                     </div>
                 </div>
-                <div className='relative left-1/3 w-[554px] h-[138px] imagem-carro'>
-                    <img src={ABTCarro} className=''/>
+                <div className='relative left-1/3 w-[544px] h-[110px] imagem-carro'>
+                    <img src={equipe.carro} className=''/>
                 </div>
 
-                <img src={Fundo} className='absolute right-0 -z-10 bottom-0 h-full'  />
+                <img src={equipe.fundo} className='absolute right-0 -z-10 bottom-0 w-3/4'/>
             </div>
+        ))}
         </>
     );
 }
---
+++ src/mocks/equipes.jsx
@@ -0,0 +1,139 @@
+import ABT from '../assets/Times FE/ABT/ABT.svg'
+import ABTCarro from '../assets/Times FE/ABT/ABT Carro.png'
+import ABTFundo from '../assets/Times FE/ABT/Fundo.svg'
+import ANDRETTI from '../assets/Times FE/ANDRETTI/Andretti.svg'
+import ANDRETTICarro from '../assets/Times FE/ANDRETTI/Andretti Carro.png'
+import ANDRETTIFundo from '../assets/Times FE/ANDRETTI/Fundo.svg'
+import DSPENSKE from '../assets/Times FE/DS PENSKE/DS Penske.svg'
+import DSPENSKECarro from '../assets/Times FE/DS PENSKE/DS Penske.png'
+import DSPENSKEFundo from '../assets/Times FE/DS PENSKE/Fundo.svg'
+import ENVISION from "../assets/Times FE/ENVISION/ENVISION.svg";
+import ENVISIONCarro from '../assets/Times FE/ENVISION/Envision Carro.png'
+import ENVISIONFundo from '../assets/Times FE/ENVISION/Fundo.svg'
+import ERT from "../assets/Times FE/ERT/ERT.svg";
+import ERTCarro from '../assets/Times FE/ERT/ERT Carro.png'
+import ERTFundo from '../assets/Times FE/ERT/Fundo.svg'
+import JAGUAR from "../assets/Times FE/JAGUAR TCS/Jaguar TCS.svg";
+import JAGUARCarro from "../assets/Times FE/JAGUAR TCS/Jaguar Carro.png";
+import JAGUARFundo from "../assets/Times FE/JAGUAR TCS/Fundo.svg";
+import MAHINDRA from "../assets/Times FE/MAHINDRA/Mahindra.svg";
+import MAHINDRACarro from "../assets/Times FE/MAHINDRA/MAHINDRA CARRO.png";
+import MAHINDRAFundo from "../assets/Times FE/MAHINDRA/Fundo.svg";
+import MASSERATI from "../assets/Times FE/MASSERATI MSG/Masserati MSG.svg";
+import MASSERATICarro from "../assets/Times FE/MASSERATI MSG/Masserati Carro.png";
+import MASSERATIFundo from "../assets/Times FE/MASSERATI MSG/Fundo.svg";
+import MCLAREN from "../assets/Times FE/NEOM MCLAREN/NEOM McLaren.svg";
+import MCLARENCarro from "../assets/Times FE/NEOM MCLAREN/NEOM McLaren carro.png";
+import MCLARENFundo from "../assets/Times FE/NEOM MCLAREN/Fundo.svg";
+import NISSAN from "../assets/Times FE/NISSAN/Nissan.svg";
+import NISSANCarro from "../assets/Times FE/NISSAN/Nissan carro.png";
+import NISSANFundo from "../assets/Times FE/NISSAN/Fundo.svg";
+import HEUER from "../assets/Times FE/TAG HEUER PORSCHE/Tag Heuer Porsche.svg";
+import HEUERCarro from "../assets/Times FE/TAG HEUER PORSCHE/Tag Heuer carro.png";
+import HEUERFundo from "../assets/Times FE/TAG HEUER PORSCHE/Fundo.svg";
+
+
+
+const equipes = [
+    {
+        time: ABT,
+        carro: ABTCarro,
+        vitorias: 14,
+        podios: 47,
+        corridas: 155,
+        fundo: ABTFundo,
+        cor: '#0099FF'
+    },
+    {
+        time: ANDRETTI,
+        carro: ANDRETTICarro,
+        vitorias: 11,
+        podios: 37,
+        corridas: 132,
+        fundo: ANDRETTIFundo,
+        cor: '#B82727'
+    },
+    {
+        time: DSPENSKE,
+        carro: DSPENSKECarro,
+        vitorias: 3,
+        podios: 17,
+        corridas: 132,
+        fundo: DSPENSKEFundo,
+        cor: '#CBA65F'
+    },
+    {
+        time: ENVISION,
+        carro: ENVISIONCarro,
+        vitorias: 16,
+        podios: 53,
+        corridas: 132,
+        fundo: ENVISIONFundo,
+        cor: '#00BE26'
+    },
+    {
+        time: ERT,
+        carro: ERTCarro,
+        vitorias: 2,
+        podios: 6,
+        corridas: 132,
+        fundo: ERTFundo,
+        cor: '#DEC800'
+    },
+    {
+        time: JAGUAR,
+        carro: JAGUARCarro,
+        vitorias: 16,
+        podios: 17,
+        corridas: 132,
+        fundo: JAGUARFundo,
+        cor: '#B1BDC5'
+    },
+    {
+        time: MAHINDRA,
+        carro: MAHINDRACarro,
+        vitorias: 5,
+        podios: 24,
+        corridas: 131,
+        fundo: MAHINDRAFundo,
+        cor: '#FF0000'
+    },
+    {
+        time: MASSERATI,
+        carro: MASSERATICarro,
+        vitorias: 10,
+        podios: 27,
+        corridas: 132,
+        fundo: MASSERATIFundo,
+        cor: '#1B4DFF'
+    },
+    {
+        time: MCLAREN, 
+        carro: MCLARENCarro,
+        vitorias: 8,
+        podios: 26,
+        corridas: 87, 
+        fundo: MCLARENFundo,
+        cor: '#FF961B'
+    },
+    {
+        time: NISSAN,
+        carro: NISSANCarro,
+        vitorias: 19,
+        podios: 47,
+        corridas: 132,
+        fundo: NISSANFundo,
+        cor: '#FF1B1B'
+    },
+    {
+        time: HEUER,
+        carro: HEUERCarro,
+        vitorias: 12,
+        podios: 22,
+        corridas: 74,
+        fundo: HEUERFundo,
+        cor: '#FF1B1B'
+    }
+]
+
+export default equipes
