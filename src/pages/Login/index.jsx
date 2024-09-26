1acee2454046578f19eee2776d55b2e74c8c1270
Leonardo Taschin <taschin21@gmail.com>
2024-09-26 16:19:16 -0300

    Responsividade Login e Register

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo/logo.svg";
+import Logo2 from "../../assets/logo/logo-expandida.svg";
 import InputLogin from "../../components/InputLogin";
 import BotaoLogin from "../../components/BotaoLogin";
 import Checkbox from "../../components/Checkbox";
@@ -7,32 +7,35 @@ import { useEffect } from "react";
 
 const Login = () => {
 
-
   useEffect(() => {
-    const tema = localStorage.getItem('theme')
+    const tema = localStorage.getItem('theme');
     if (tema === 'dark'){
-      document.body.classList.add('dark')
+      document.body.classList.add('dark');
     }
-  }, [])
+  }, []);
 
   return (
     <>
       <div className="flex justify-center py-12 border-b-black border-y">
-        <img src={Logo} className="w-1/6"/>
+        <img src={Logo2} className="w-1/ md:w-1/4 lg:w-1/5" alt="Logo" />
       </div>
-      <form className="flex flex-col justify-center items-center h-full">
-        <div className="flex flex-col items-center gap-2 dark:text-white">
-          <h1 className="text-3xl font-medium pt-5">Login</h1>
-          <p>Não possui uma conta? <Link to={'/register'} className="gradient-text">Cadastre-se</Link></p>
-        </div>
-        <div className="w-full">
+      <div className="max-w-md mx-auto px-4">
+        <form className="flex flex-col justify-center items-center">
+          <div className="flex flex-col items-center gap-4 dark:text-white">
+            <h1 className="text-3xl md:text-4xl font-medium pt-5">Login</h1>
+            <p className="text-center">
+              Não possui uma conta? <Link to={'/register'} className="gradient-text">Cadastre-se</Link>
+            </p>
+          </div>
+          <div className="w-full mt-4">
             <InputLogin placeholder={'E-mail'} />
             <InputLogin placeholder={'Senha'} eye className='mb-2'/>
             <label className="gradient-text text-sm cursor-pointer">Esqueceu sua senha?</label>
-        </div>
-            <Checkbox texto={'Lembrar de mim?'}/>
-            <BotaoLogin texto={'Entrar'}/>
-      </form>
+          </div>
+          <Checkbox texto={'Lembrar de mim?'}/>
+          <BotaoLogin texto={'Entrar'}/>
+        </form>
+      </div>
     </>
   );
 };
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo/logo.svg";
+import Logo2 from "../../assets/logo/logo-expandida.svg";
 import InputLogin from "../../components/InputLogin";
 import Checkbox from "../../components/Checkbox";
 import BotaoLogin from "../../components/BotaoLogin";
@@ -8,31 +8,35 @@ import { useEffect } from "react";
 const Register = () => {
 
   useEffect(() => {
-    const tema = localStorage.getItem('theme')
-    if (tema === 'dark'){
-      document.body.classList.add('dark')
+    const tema = localStorage.getItem('theme');
+    if (tema === 'dark') {
+      document.body.classList.add('dark');
     }
-  }, [])
+  }, []);
+
   return (
     <>
-      <div className="flex justify-center py-12 border-y border-b-black dark: border-white">
-        <img src={Logo} className="w-1/6" />
+      <div className="flex justify-center py-12 border-y border-b-black dark:border-white">
+        <img src={Logo2} className="w-1/ md:w-1/4 lg:w-1/5" alt="Logo" />
       </div>
-      <form>
-        <div className="flex flex-col items-center gap-2 dark:text-white">
-          <h1 className="text-3xl font-medium pt-5">Registrar</h1>
-          <p>Já possui uma conta? <Link to={'/login'} className="gradient-text">Logar</Link></p>
-        </div>
-        <div className="w-full">
+      <div className="max-w-md mx-auto px-4">
+        <form>
+          <div className="flex flex-col items-center gap-4 dark:text-white">
+            <h1 className="text-3xl md:text-4xl font-medium pt-5">Registrar</h1>
+            <p className="text-center">
+              Já possui uma conta? <Link to={'/login'} className="gradient-text">Logar</Link>
+            </p>
+          </div>
+          <div className="w-full mt-4">
             <InputLogin placeholder={'E-mail'} />
-            <InputLogin placeholder={'Nome de usuário'}  className='mb-2'/>
-            <InputLogin placeholder={'Senha'} eye className='mb-2'/>
-            <InputLogin placeholder={'Confirmar senha'} eye className='mb-2'/>
+            <InputLogin placeholder={'Nome de usuário'} className='mb-2' />
+            <InputLogin placeholder={'Senha'} eye className='mb-2' />
+            <InputLogin placeholder={'Confirmar senha'} eye className='mb-2' />
             <Checkbox texto={'Desejo receber atualizações e ofertas em seu e-mail'} />
             <BotaoLogin texto={'Registrar'} />
-
-        </div>
-      </form>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
